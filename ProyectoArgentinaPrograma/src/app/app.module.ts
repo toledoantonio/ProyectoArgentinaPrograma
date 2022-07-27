2ce17eec41b915946e286c088fc89c970e1d35f0
toledoantonio <antonioramont7@gmail.com>
2022-07-26 23:26:09 -0300

    Se agrego banner,BS y acerca de

--- ProyectoArgentinaPrograma/src/app/app.module.ts
+++ ProyectoArgentinaPrograma/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    RedesComponent,
+    AboutMeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
